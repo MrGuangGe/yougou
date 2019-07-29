9185116d7cda6d596abaaddb80d1c2dd65b0c1bd
Mr <ninhao1002@163.com>
2019-07-29 15:06:45 +0800

    获取导航分类列表数据并渲染完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,16 @@
 Page({
   data: {
     // 轮播图的数据
-    swiperList: []
+    swiperList: [],
+    // 分类导航数据
+    navCateList: []
   },
   onLoad: function (options) {
     this.getSwiperList()
+    this.getNavCateList()
   },
   // 发送请求获取轮播图数据
-  getSwiperList(){
+  getSwiperList() {
     wx.request({
       url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
       method: 'GET',
@@ -16,6 +19,20 @@ Page({
         let { message } = res.data
         this.setData({
           swiperList: message
+        })
+      }
+    })
+  },
+  // 发送请求获取分类导航的数据
+  getNavCateList() {
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
+      method: 'GET',
+      success: (res) => {
+        // console.log(res)
+        let { message } = res.data
+        this.setData({
+          navCateList: message
         })
       }
     })
