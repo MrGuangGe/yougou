914ed6f5bdc206ad06c174c2074d167fd627ffac
Mr <ninhao1002@163.com>
2019-07-29 17:30:07 +0800

    分类-获取左右两侧滚动数据完成 下一步渲染

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,30 @@
-// pages/category/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    // 左侧栏数据
+    leftList: [],
+    // 右侧栏数据
+    rightList: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this.getLeftList()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取左侧栏数据
+  getLeftList() {
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/categories',
+      method: 'GET',
+      success: (res) => {
+        // console.log(res.data)
+        const { message } = res.data
+        // 左侧栏数据
+        let leftList = message.map(val => ({ cat_id: val.cat_id, cat_name: val.cat_name }))
+        // 右侧栏数据
+        let rightList = message[0].children
+        this.setData({
+          leftList,
+          rightList
+        })
+      }
+    })
   }
 })
