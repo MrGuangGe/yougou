34ff6a10fd0d174011886668b2f71d7699a051a7
Mr <ninhao1002@163.com>
2019-07-31 17:28:51 +0800

    商品列表-请求数据回来渲染页面

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,11 +1,42 @@
-// pages/goods_list/index.js
+// 引入封装好的发送异步请求的方法 promise
+import { request } from "../../request/index.js"
+
 Page({
   data: {
-
+    // 商品列表的数据
+    goodsList: []
   },
   // 页面加载触发
   onLoad: function (options) {
+    // 通过形参options获取到url上的参数
+    // console.log(options)
+    this.queryParams.cid = options.cid
 
+    this.getGoodsList()
+  },
+  // 发送请求商品列表需要带上的参数
+  queryParams: {
+    query: "",
+    cid: 1,
+    pagenum: 1,
+    pagesize: 10
+  },
+  // 获取商品列表的数据
+  getGoodsList() {
+    request({
+      url: "/goods/search",
+      method: "GET",
+      data: this.queryParams
+    })
+      .then(res => {
+        // console.log(res.data)
+        this.setData({
+          goodsList: res.data.message.goods
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
   },
   // 下拉刷新触发
   onPullDownRefresh: function () {
@@ -13,6 +44,6 @@ Page({
   },
   // 滚动条触底触发
   onReachBottom: function () {
-    
+
   },
 })
