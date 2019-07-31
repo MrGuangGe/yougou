2d0b94bd8ae7780a9e9713a0655d577607f6a2cc
Mr <ninhao1002@163.com>
2019-07-31 09:36:31 +0800

    分类-切换菜单项改变右侧栏的内容

--- pages/category/index.js
+++ pages/category/index.js
@@ -8,8 +8,12 @@ Page({
     // 右侧栏数据
     rightList: [],
     // 初始索引值，为了添加类active的样式给选中的左侧栏
-    currentIndex: 0
+    currentIndex: 0,
+    // 改变左侧栏菜单选项 右侧内容置顶
+    scrollTop: 0
   },
+  // 改变左侧栏菜单选项 右侧内容相应改变
+  cates: [],
   onLoad: function (options) {
     this.getLeftList()
   },
@@ -22,6 +26,8 @@ Page({
       .then(res => {
         // console.log(res.data)
         const { message } = res.data
+        // 把请求回来的数据赋值给全局变量cates
+        this.cates = message
         // 左侧栏数据
         let leftList = message.map(val => ({ cat_id: val.cat_id, cat_name: val.cat_name }))
         // 右侧栏数据
@@ -39,8 +45,13 @@ Page({
   hadChoseItem(event) {
     // console.log(event)
     const { index } = event.currentTarget.dataset
+    // 点击左侧菜单项改变右边相应的内容
+    let rightList = this.cates[index].children
     this.setData({
-      currentIndex: index
+      currentIndex: index,
+      rightList,
+      // 点击左侧栏菜单选项 重新激活scrollTop
+      scrollTop: 0
     })
   }
 })
