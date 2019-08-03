42643deedf6f8975e890e9099c85f0c0a1dd4f3d
Mr <ninhao1002@163.com>
2019-08-03 20:07:01 +0800

    支付-静态布局+动态渲染数据

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,66 +1,49 @@
-// pages/pay/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    // 商品信息
+    goodsInfo: {},
+    // 收货地址
+    address: {},
+    totalPrice: 0,
+    totalNum: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
+  // 页面切换都会触发
   onShow: function () {
+    // 从本地存储中获取地址信息
+    let address = wx.getStorageSync("address") || {}
+    // 添加一个all属性 用来拼接地址栏
+    address.all = address.provinceName + address.cityName + address.countyName + address.detailInfo
+    // 赋值给data中的address
+    this.setData({ address })
 
+    // 从本地存储中获取商品信息
+    let cartData = wx.getStorageSync("cart_add") || {}
+    // 调用setCartGoodsData
+    this.setCartGoodsData(cartData)
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  // 重置购物车商品信息 计算总价格
+  setCartGoodsData(cartData) {
+    // 1.把cartData转换为数组
+    let cartArr = Object.values(cartData)
+    // 2.计算总价格
+    let totalPrice = 0
+    // 3.计算总数量
+    let totalNum = 0
+    cartArr.forEach(val => {
+      if (val.checked) {
+        // 总价格
+        totalPrice += val.num * val.goods_price
+        // 总数量
+        totalNum += val.num
+      }
+    })
+    // 修改data中的数据
+    this.setData({ goodsInfo: cartData, totalPrice, totalNum })
+    // 防止数据改变了 刷新之后没有效果 所以也顺便存入到缓存中
+    wx.setStorageSync('cart_add', cartData)
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 支付的点击事件
+  pay() {
+    console.log("支付")
   }
 })
