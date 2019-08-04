16920f13b0a9b633d6c92eb9f1539661b1a5235a
Mr <ninhao1002@163.com>
2019-08-04 18:45:13 +0800

    支付-完成支付

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,5 +1,9 @@
 // 引入封装好的本地存储的文件
 import { setStorageSyncCart, getStorageSyncCart, getStorageSyncAddress, getStorageSyncToken } from "../../utils/storage.js"
+// 引入封装好的发送异步请求的方法 promise
+import { request } from "../../request/index.js"
+import { showToast, requestPayment } from "../../utils/asyncWX.js"
+import regeneratorRuntime from '../../lib/runtime/runtime.js'
 
 Page({
   data: {
@@ -46,16 +50,86 @@ Page({
     setStorageSyncCart(cartData)
   },
   // 支付的点击事件
-  pay() {
-    const token = getStorageSyncToken()
-    // 判断 是否存在token值
-    // 1.不存在 跳转到授权页面
-    if (!token) {
-      wx.navigateTo({
-        url: '/pages/auth/index'
-      })
-    } else {  // 2. 存在 进行支付的业务处理
-      console.log("支付")
+  async pay() {
+    try {
+      const token = getStorageSyncToken()
+      // 判断 是否存在token值
+      // 1.不存在 跳转到授权页面
+      if (!token) {
+        wx.navigateTo({
+          url: '/pages/auth/index'
+        })
+        return
+      } else {  // 2. 存在 进行支付的业务处理
+        /**
+         * 2.1 创建订单  就是获取订单编号order_number
+         *    请求头参数：Authorization
+         *    请求体参数：order_price、consignee_addr、goods[goods_id,goods_number,goods_price]
+         */
+        // 2.2 设置请求头
+        const header = { Authorization: token }
+        // 2.2 获取请求体参数 order_price、consignee_addr、goods[goods_id,goods_number,goods_price]
+        // 订单总价格
+        const order_price = this.data.totalPrice
+        // 收货地址
+        const consignee_addr = this.data.address.all
+        // 订单数组
+        let goods = []
+        const { goodsInfo } = this.data
+        let cartArr = Object.values(goodsInfo)
+        cartArr.forEach(val => {
+          if (val.checked) {
+            goods.push({
+              goods_id: val.goods_id,  // 商品id
+              goods_number: val.num,   // 购买商品数量
+              goods_price: val.goods_price  // 商品价格
+            })
+          }
+        })
+        // 2.3 请求参数合体
+        const params = { order_price, consignee_addr, goods }
+        // 2.4 发送请求
+        const res1 = await request({
+          url: "/my/orders/create",
+          method: "POST",
+          header,
+          data: params
+        })
+        // 拿到订单编号
+        let { order_number } = res1.data.message
+
+        /**
+         * 3.1 获取支付参数 pay
+         *      请求头参数：Authorization
+         *      请求体参数：order_number
+         */
+        // 3.2 发送请求
+        const res2 = await request({
+          url: "/my/orders/req_unifiedorder",
+          method: "POST",
+          header,
+          data: { order_number }
+        })
+        const { pay } = res2.data.message
+
+        /**
+         * 4.1 调起系统的微信支付就真的会扣钱了 requestPayment()
+         */
+        await requestPayment(pay)
+
+        /**
+         * 5.1 查询订单状态来确认是否真的支付成功
+         */
+        await request({
+          url: "/my/orders/chkOrder",
+          method: "POST",
+          data: { order_number }
+        })
+        await showToast({ title: "支付成功！" })
+      }
+    } catch (err) {
+      await showToast({ title: "支付失败！" })
+      console.log(err)
     }
   }
 })
--- utils/asyncWX.js
+++ utils/asyncWX.js
@@ -13,3 +13,42 @@ export const wxlogin = () => {
         })
     })
 }
+
+/**
+ * 调用微信系统内的支付（会扣钱哦！）
+ * @param {object} pay 支付要的参数对象
+ */
+export const requestPayment = (pay) => {
+    return new Promise((resolve, reject) => {
+        wx.requestPayment({
+            ...pay,
+            success: (res) => {
+                resolve(res)
+            },
+            fail: (err) => {
+                reject(err)
+            }
+        })
+    })
+}
+
+/**
+ * wx.showToast
+ */
+export const showToast = ({ title }) => {
+    return new Promise((resolve, reject) => {
+        wx.showToast({
+            title: title,
+            icon: 'none',
+            image: '',
+            duration: 1500,
+            mask: false,
+            success: (res) => {
+                resolve(res)
+            },
+            fail: (err) => {
+                reject(err)
+            }
+        })
+    })
+}
