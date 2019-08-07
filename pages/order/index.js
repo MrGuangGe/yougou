fa32bda78bdf7a3e523ff1a79e7d55dc7638092f
Mr <ninhao1002@163.com>
2019-08-07 08:40:35 +0800

    订单-静态布局

--- pages/order/index.js
+++ pages/order/index.js
@@ -1,66 +1,38 @@
-// pages/order/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    // tabs栏的数据
+    tabs: [
+      {
+        id: 1,
+        value: "全部",
+        isActive: true
+      },
+      {
+        id: 2,
+        value: "代付款",
+        isActive: false
+      },
+      {
+        id: 3,
+        value: "代发货",
+        isActive: false
+      },
+      {
+        id: 4,
+        value: "退款/退货",
+        isActive: false
+      }
+    ],
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 子组件调用这个函数
+  handleAcceptIndex(event) {
+    // console.log(event)
+    const { index } = event.detail
+    // 获取源数组
+    let { tabs } = this.data;
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false)
+    this.setData({
+      tabs
+    })
   }
 })
